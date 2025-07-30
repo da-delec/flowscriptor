f5d6a045a5f62cc21b0eee277a9da2027d20730b
Corentin Delesalle <corentindelesalle@MacBook-Air-de-Corentin.local>
2025-07-30 19:02:58 +0200

    correctif bug route get

--- app/api/get_user_script/[id]/route.ts
+++ app/api/get_user_script/[id]/route.ts
@@ -3,7 +3,10 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET (req:Request,{params}:{params:{id:string}}){
+export async function GET (
+    req: Request,
+    { params }: { params: { id: string } }
+  ) {
     const {id} = await params
     console.log(id)
     const userScript = await prisma.user.findUnique({
