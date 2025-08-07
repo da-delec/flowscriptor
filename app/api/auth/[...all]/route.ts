2c8c169acf489cfa904a4ed34088e71a9925b9f8
Corentin Delesalle <delesallecorentin3@gmail.com>
2025-08-07 18:09:41 +0200

    same bug

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -2,14 +2,4 @@ import { auth } from "@/lib/auth"; // path to your auth file
 import { toNextJsHandler } from "better-auth/next-js";
 import { NextRequest } from "next/server";
 
-const handler = toNextJsHandler(auth);
-
-export async function GET(req: NextRequest) {
-  console.log("🔍 API Auth GET:", req.url);
-  return handler.GET(req);
-}
-
-export async function POST(req: NextRequest) {
-  console.log("🔍 API Auth POST:", req.url);
-  return handler.POST(req);
-}
+export const { POST, GET } = toNextJsHandler(auth);
--- lib/auth.ts
+++ lib/auth.ts
@@ -112,9 +112,9 @@ export const auth = betterAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
         },
         google: {
-            clientId: process.env.GOOGLE_CLIENT_ID! as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET! as string,
-            disableRedirect: false, // Permettre la redirection automatique
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+             // Permettre la redirection automatique
         }
     },
 
